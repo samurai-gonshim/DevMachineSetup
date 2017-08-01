8527da356a4748608c9d778c028289191b98465e
samurai-gonshim <gonshim@gmail.com>
2017-08-01 18:32:58 +0900

    initial commit

---
+++ .oh-my-zsh/custom/gonaliases.zsh
@@ -0,0 +1,40 @@
+#------------------
+# personal ENV
+#------------------
+
+
+#------------------
+# personal aliases
+#------------------
+
+alias a='alias'
+alias v='vim'
+alias vi='vim'
+alias cls='clear'
+alias so='source'
+alias sozsh='source ~/.zshrc'
+#alias termh='gnome-terminal --working-directory="~"'
+#alias term='gnome-terminal'
+#alias nau='nautilus'	# file browser
+#alias subl='sublime'
+alias h='history'
+alias code='cd ~/Code'
+
+#-------------
+# git aliases
+#--------------
+
+alias ga='git add'
+alias gs='git status'
+alias gl='git log'
+alias gco='git checkout'
+alias gm='git commit -m'
+
+#----
+# pbcopy
+# ---
+if which xsel >/dev/null 2>&1 ; then
+   # linux
+   alias pbcopy='xsel --input --clipboard'
+   alias pbpaste='xsel --output --clipboard'
+fi
